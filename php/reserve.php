1a6a575821d1134da7074bbcca0071aba80374fc
ANDRII-PC\Andrii <davudiykandy1994@gmail.com>
2018-01-17 10:52:28 +0200

    first-commit

---
+++ php/reserve.php
@@ -0,0 +1,33 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+try {
+    
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'ssl://smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'mejison8@gmail.com';                 // SMTP username
+    $mail->Password = 'knopka100';                           // SMTP password
+    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('mejison8@gmail.com', 'Mailer');
+    $mail->addBCC('loviwasabo@letsmail9.com');
+
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+}
